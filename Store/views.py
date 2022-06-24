5256954817a4e5d912fe50e22571c4bc7a25257b
Janet <janetmutua04@gmail.com>
2022-06-24 22:38:12 +0300

    add TemplateView

--- Store/views.py
+++ Store/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import *
 from django.views.generic import ListView, DetailView
-
+from django.views.generic.base import TemplateView
 
 
 # Create your views here.
@@ -38,38 +38,38 @@ class Trending(ListView):
      template_name = 'Store/trending.html'
 
 
-class Clearance(ListView):
-    context_object_name = 'clearance'
-    queryset = Product.objects.filter(clearance_sale= 1)
+class Clearance(TemplateView):
     template_name = 'Store/clearance.html'
 
 
     def get_context_data(self, *args, **kwargs):
-        cat_menu = Category.objects.filter(status=0)
+        cat_menu = Category.objects.all()
+        clearance_sale = Product.objects.filter(clearance_sale= 1)
         plussize = Category.objects.filter(PlusSize = 1)
         clearance = Category.objects.filter(Clearance= 1)
         petite = Category.objects.filter(Petite = 1)
 
         context =  super(Clearance, self).get_context_data(*args, **kwargs)
-        context = {'cat_menu': cat_menu,'plussize': plussize,'clearance': clearance, 'petite':petite}  
+        context = {'cat_menu': cat_menu, 'clearance_sale': clearance_sale, 'plussize':plussize, 'petite':petite, 'clearance': clearance}  
             
         return context
 
 
-class NewArrival(ListView):
-    context_object_name = 'new'
-    queryset = Product.objects.filter(new_arrival = 1)
+class NewArrival(TemplateView):
+    model = Product
     template_name = 'Store/newarrivals.html'
+    paginate_by = 2
 
 
     def get_context_data(self, *args, **kwargs):
-        cat_menu = Category.objects.filter(status=0)
+        cat_menu = Category.objects.all()
+        new = Product.objects.filter(new_arrival = 1)
         plussize = Category.objects.filter(PlusSize = 1)
         clearance = Category.objects.filter(Clearance= 1)
         petite = Category.objects.filter(Petite = 1)
 
         context =  super(NewArrival, self).get_context_data(*args, **kwargs)
-        context = {'cat_menu': cat_menu,'plussize': plussize,'clearance': clearance, 'petite':petite}  
+        context = {'cat_menu': cat_menu, 'new': new, 'plussize':plussize, 'petite':petite, 'clearance': clearance}  
             
         return context
 
